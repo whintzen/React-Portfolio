26f05a4181ded24218e21ea2f6ba42c3d8284d38
Wendy Hintzen <wpah@comcast.net>
2020-11-29 23:43:40 -0500

    Added the LinkedIn image to index.js and updated style.css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import Container from "../../components/Container";
 import Col from  "../../components/Col";
 import Row from "../../components/Row";
+// import Anchor from "../../components/Anchor";
+import { SocialIcon } from 'react-social-icons';
 import "./style.css";
 
 function Home() {
@@ -10,7 +12,7 @@ function Home() {
         <Container>
             <Row>
                 <Col size="lg-12">                     
-                    <h1 class="display-5">My Portfolio</h1>                        
+                    <h1 className="display-5">My Portfolio</h1>                        
                         <hr /> 
                 </Col>                         
                 <Col size="md-12">                                       
@@ -18,7 +20,19 @@ function Home() {
                         className="img-fluid"/>             
                 </Col>
             </Row>
+            
         </Container>
+        <Row ClassName="text-center">
+                <Col size="12">                
+                    <hr className="socialimg"/>
+                    <h2>Connect</h2>                
+                </Col>
+                <Col size="12 social padding">
+                    <SocialIcon url="https://linkedin.com/in/wendy-hintzen-999b7583/" /> 
+                    {/* <Anchor href={"https://www.linkedin.com/in/wendy-hintzen-999b7583/"} target="_blank" rel="noreferrer" class="fa fa-linkedin">  - LinkedIn </Anchor>                                    */}
+                    {/* <a href="https://www.linkedin.com/in/wendy-hintzen-999b7583/" target="_blank" rel="noreferrer" class="fa fa-linkedin">   My LinkedIn</a>                                  */}
+                </Col>
+        </Row>
        </> 
     );
 }
