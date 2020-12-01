3a8cf60ea90818f2e2480d62163d347464d71b13
Wendy Hintzen <wpah@comcast.net>
2020-12-01 15:48:30 -0500

    Updated About index.js and Aboutstyle.css to add list items

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -31,16 +31,37 @@ function About() {
                 <Col size="lg-6">
                               
                     <p id="Contact"> <strong>Contact Info:</strong></p>
-                    <ul>                            
-                        <li><strong>Email:</strong> <a href="/">wpah@comcast.net</a></li>
+                    {/* <ul>                             */}
+                        {/* <li><strong>Email:</strong> <a href="/">wpah@comcast.net</a></li>
                         <li><strong>Telephone:</strong> <a href="/">973-673-2555</a></li>
                         <li><strong>Resume:</strong> <a href="./assets/resume/Wendy_Hintzen.pdf" target="_blank" rel="noreferrer" alt="wendyresume">Wendy Hintzen's Resume</a></li>
-                        <li><strong>GitHub:</strong> <a href="https://github.com/whintzen" target="_blank" rel="noreferrer">github.com/whintzen</a></li> 
+                        <li><strong>GitHub:</strong> <a href="https://github.com/whintzen" target="_blank" rel="noreferrer">github.com/whintzen</a></li>  */}
+                    {/* </ul> */}
+                    <ul>
+                        <strong>Email:</strong> <a href="/">wpah@comcast.net</a>
+                        <br />
+                        <strong>Telephone:</strong> <a href="/">973-673-2555</a>
+                        <br />
+                        <strong>Resume:</strong> <a href="./assets/resume/Wendy_Hintzen.pdf" target="_blank" rel="noreferrer" alt="wendyresume">Wendy Hintzen's Resume</a>
+                        <br />
+                        <strong>GitHub:</strong> <a href="https://github.com/whintzen" target="_blank" rel="noreferrer" alt="Github">github.com/whintzen</a> 
                     </ul>
                 
                 </Col>
                 </Row>  
-            </Container>     
+            </Container>
+            <Row className="text-center">
+                <Col size="md-12">                
+                    <hr className="socialhr"/>
+                    <h2 className="connect">Connect</h2>                
+                </Col>
+                
+                <Col size="md-4"></Col>
+                <Col size="md-4 social">             
+                    <a href="https://www.linkedin.com/in/wendy-hintzen-999b7583/" target="_blank" rel="noreferrer" className="fa fa-linkedin"> </a>                                 
+                </Col>
+                <Col size="md-4"></Col>                
+            </Row>     
         </div>
      );
 }
