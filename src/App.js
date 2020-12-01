9ec07e020f200dfb1979205cc87a7eae7bd5b91e
Wendy Hintzen <wpah@comcast.net>
2020-11-30 19:52:45 -0500

    Updated App.js to add the About Page link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+// import './App.css';
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
-// import About from "./pages/About";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
         <Wrapper>
           
             <Route exact path="/" component={Home} />
-            {/* <Route exact path="/about" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} /> */}
+            <Route exact path="/about" component={About} />
+            {/* <Route exact path="/portfolio" component={Portfolio} /> */}
 
         </Wrapper>
       </div>
