dfb83d1f78267d6703eafd3cfeedff2fadae39e9
Wendy Hintzen <wpah@comcast.net>
2020-12-01 22:47:19 -0500

    Updated App.js to remove the Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import './App.css';
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+
 
 function App() {
   return (
     <Router>
       <div className="App">        
-        <Navbar />
         <Wrapper>
           
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
-            {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+            <Route exact path="/portfolio" component={Portfolio} />
 
         </Wrapper>
       </div>
