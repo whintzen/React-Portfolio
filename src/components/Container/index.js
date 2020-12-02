783b7af73e4d0fd8560839e0e5c29b99fd228bed
Wendy Hintzen <wpah@comcast.net>
2020-12-02 04:20:54 -0500

    Updates

--- src/components/Container/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-function Container(props) {
-  return <div className="container-body padding" style={props.style}>{props.children}</div>;
-}
-
-export default Container;
--- src/components/Row/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Row(props) {
-  return <div className="row padding">{props.children}</div>;
-}
-
-export default Row;
